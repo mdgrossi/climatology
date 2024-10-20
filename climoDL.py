495e1ef5e84ce3c63abcd0d2b52cd9c05530f291
mdgrossi <35579673+mdgrossi@users.noreply.github.com>
2024-10-19 22:47:54 -0400

    Change formatting

--- climoDL.py
+++ climoDL.py
@@ -9,7 +9,7 @@
 # locally, it is updated with the most recently available observations.
 #
 # To execute for a new station:
-# python climoDL.py -s "Virginia Key, FL" -i "8723214" -u "english" -t "lst" -d "MHHW" --hr 3 --day 2 
+# python3 climoDL.py -s "Virginia Key, FL" -i "8723214" -u "english" -t "lst" -d "MHHW" --hr 3 --day 2 
 #
 #  NEED TO IMPLEMENT:
 #  - Commit and push new data to GitHub to trigger gh-pages
@@ -26,41 +26,57 @@ import os
 
 # -----------------------------------------------------------------------------
 # FUNCTIONS
+
 def parse_args():
     """Parse command line arguments"""
-    parser = argparse.ArgumentParser(description='Function control parameters.',
-                        prog='climoDL',
-                        usage='%(prog)s [arguments]')
-    parser.add_argument('-s', '--station', metavar='station', type=str,
-                        help='Desired name of station. Used for saving data.',
-                        default=None)
-    parser.add_argument('-i', '--id', metavar='stationid', type=str,
-                        help='Tide station number from which to retrieve data.',
-                        default=None)
-    parser.add_argument('-o', '--outdir', metavar='outdir', type=str,
-                        help='Directory to save data to.',
-                        default=None)
-    parser.add_argument('-u', '--units', metavar='units', type=str,
-                      help='Data units, either "metric" or "english".',
-                      default='english')
-    parser.add_argument('-t', '--timezone', metavar='timezone', type=str,
-                      help='Timezone, either "gmt", "lst", or "lst_ldt".',
-                      default='lst')
-    parser.add_argument('-d', '--datum', metavar='datum', type=str,
-                        help='Tidal datum for water level data. Options: '+
-                             '"STND", "MHHW", "MHW", "MTL", "MSL", "MLW", '+
-                             '"MLLW", "NAVD"',
-                        default='MHHW')
-    parser.add_argument('--hr', metavar='hr_threshold', type=int,
-                        help='Max number of hours of data that can be missing.',
-                        default=3)
-    parser.add_argument('--day', metavar='day_threshold', type=int,
-                        help='Max number of days of data that can be missing.',
-                        default=2)
-    parser.add_argument('-r', '--redownload', action='store_true',
-                      help='Force redownload of historical data.')
-    parser.add_argument('-v', '--verbose', action='store_true',
-                      help='Print statuses to screen.')
+    parser = argparse.ArgumentParser(
+        prog='climoDL',
+        usage='%(prog)s [arguments]',
+        description='Function control parameters.')
+    parser.add_argument(
+        '-s', '--station',
+        metavar='station', type=str,
+        default=None,
+        help='Desired name of station. Used for saving data.')
+    parser.add_argument(
+        '-i', '--id',
+        metavar='stationid', type=str,
+        default=None,
+        help='Tide station number from which to retrieve data.')
+    parser.add_argument(
+        '-u', '--units',
+        metavar='units', type=str,
+        default='english',
+        help='Data units, either "metric" or "english".')
+    parser.add_argument(
+        '-t', '--timezone',
+        metavar='timezone', type=str,
+        default='lst',
+        help='Timezone, either "gmt", "lst", or "lst_ldt".')
+    parser.add_argument(
+        '-d', '--datum',
+        metavar='datum', type=str,
+        default='MHHW',
+        help='Tidal datum for water level data. Options: '+
+             '"STND", "MHHW", "MHW", "MTL", "MSL", "MLW", "MLLW", "NAVD"')
+    parser.add_argument(
+        '--hr',
+        metavar='hr_threshold', type=int,
+        default=3,
+        help='Max number of hours of data that can be missing.')
+    parser.add_argument(
+        '--day',
+        metavar='day_threshold', type=int,
+        default=2,
+        help='Max number of days of data that can be missing.')
+    parser.add_argument(
+        '-r', '--redownload',
+        action='store_true',
+        help='Force redownload of historical data.')
+    parser.add_argument(
+        '-v', '--verbose',
+        action='store_true',
+        help='Print statuses to screen.')
     return parser.parse_args()
 
 # =============================================================================
@@ -69,10 +85,8 @@ def parse_args():
 def main():
     # Parse command line arguments
     args = parse_args()
-    if not args.outdir:
-        args.outdir = os.getcwd()
-
-    # Download data
+    
+    # Download data and update stats
     data = Data(
         stationname=args.station,
         stationid=args.id,
@@ -88,4 +102,5 @@ def main():
     data.update_stats()
 
 if __name__ == "__main__":
+    """Main program"""
     main()
